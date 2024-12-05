6dd569bd47642ab97cf56cefc758e917c96975f3
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-12-05 11:22:11 +0530

    chg animation speed

--- js/main.js
+++ js/main.js
@@ -266,18 +266,18 @@ ScrollReveal({
 });
 
 /* target elements and specific options to create reveal animation */
-ScrollReveal().reveal('.home .info h2', { delay: 500, origin: 'left' })
-ScrollReveal().reveal('.home .info h2, .section-title-01, .section-title-02', { delay: 500, origin: 'left' })
-ScrollReveal().reveal('.home .info h3, .home .info .p, .about-info .btn', { delay: 600, origin: 'right' })
-ScrollReveal().reveal('.home .info .btn', { delay: 700, origin: 'bottom' })
-ScrollReveal().reveal('.media-icons i, .contact-left li', { delay: 500, origin: 'left', interval: 200 })
+ScrollReveal().reveal('.home .info h2', { delay: 100, origin: 'left' })
+ScrollReveal().reveal('.home .info h2, .section-title-01, .section-title-02', { delay: 100, origin: 'left' })
+ScrollReveal().reveal('.home .info h3, .home .info .p, .about-info .btn', { delay: 100, origin: 'right' })
+ScrollReveal().reveal('.home .info .btn', { delay: 100, origin: 'bottom' })
+ScrollReveal().reveal('.media-icons i, .contact-left li', { delay: 100, origin: 'left', interval: 200 })
 ScrollReveal().reveal('.home-img, about-img', { delay: 500, origin: 'bottom' })
-ScrollReveal().reveal('.about description, .contact-right', { delay: 600, origin: 'right' })
-ScrollReveal().reveal('.about .professional-list li', { delay: 500, origin: 'right', interval: 200 })
-ScrollReveal().reveal('.skills-description, .services-description, .contact-card, .client-swiper', { delay: 500, origin: 'right' })
-ScrollReveal().reveal('.skills-description, .services-description, .contact-card, .client-swiper, .contact-left h2', { delay: 700, origin: 'left' })
-ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio .img-card', { delay: 800, origin: 'bottom', interval: 200 })
-ScrollReveal().reveal('.footer .group', { delay: 500, origin: 'top', interval: 200 })
+ScrollReveal().reveal('.about description, .contact-right', { delay: 100, origin: 'right' })
+ScrollReveal().reveal('.about .professional-list li', { delay: 100, origin: 'right', interval: 200 })
+ScrollReveal().reveal('.skills-description, .services-description, .contact-card, .client-swiper', { delay: 100, origin: 'right' })
+ScrollReveal().reveal('.skills-description, .services-description, .contact-card, .client-swiper, .contact-left h2', { delay: 100, origin: 'left' })
+ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio .img-card', { delay: 100, origin: 'bottom', interval: 200 })
+ScrollReveal().reveal('.footer .group', { delay: 100, origin: 'top', interval: 200 })
 // 
 
 
